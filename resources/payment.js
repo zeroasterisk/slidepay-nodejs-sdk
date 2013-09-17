ca087cec1bdaf803c85eefcc2391288f314e0e96
Ben Drucker <bvdrucker@gmail.com>
2013-09-17 10:39:25 -0400

    Payment resource

---
+++ resources/payment.js
@@ -0,0 +1,25 @@
+/*
+@module resources/payments
+SlidePay simple and stored payments
+*/
+
+var generate = require('./generate');
+
+module.exports = function(client) {
+	var baseResourceUrl = '/payment';
+
+	// Instance functions
+	function payment(id) {
+		return {
+			read: generate(client, 'GET', baseResourceUrl + '/' + id),
+			refund: generate(client, 'POST', baseResourceUrl + '/refund/' + id)
+		};
+	}
+
+	// List functions
+	payment.create = generate(client, 'POST', baseResourceUrl + '/simple');
+	payment.store = generate(client, 'POST', '/stored_payment');
+	payment.search = generate(client, 'PUT', baseResourceUrl);
+
+	return payment;
+};
