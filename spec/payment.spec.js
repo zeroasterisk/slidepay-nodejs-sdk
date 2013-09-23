674f2d7ce6261c10d1a9e0bbeb9423b6b7e9b616
Ben Drucker <bvdrucker@gmail.com>
2013-09-23 12:06:43 -0400

    Full test coverage for SlidePay test credit card values (https://getcube.atlassian.net/wiki/display/CDP/Test+Credit+Cards+and+Payment+Values). Card types with multiple numbers use only the first provided number.

---
+++ spec/payment.spec.js
@@ -0,0 +1,79 @@
+var _			= require('lodash'),
+	should		= require('chai').should(),
+	slidepay	= require('../index.js'),
+	credentials	= require('../credentials.json');
+
+describe('A SlidePay payment', function() {
+	var SlidePay = new slidepay.RestClient(credentials);
+	this.timeout(5000);
+
+	before(function(done) {
+		SlidePay.login().then(function() {
+			done();
+		});
+	});
+
+	genericPayment = {
+		method: 'CreditCard',
+		amount: 1.00,
+		cc_expiry_month: '12',
+		cc_expiry_year: '2020'
+	};
+
+	passingGenericPayment = _.extend(genericPayment, {
+		cc_billing_zip: '11111'
+	});
+
+	var testCards = {
+		'Visa': '4012888888881881',
+		'MasterCard': '5454545454545454',
+		'AmericanExpress': '371449635398456',
+		'Discover': '6011000995504101'
+	};
+
+	_.each(testCards, function(cardNumber, cardType) {
+		it('should successfully process a ' + cardType + ' card', function(done) {
+
+			simplePayment = _.extend(passingGenericPayment, {
+				cc_number: cardNumber
+			});
+
+			SlidePay.payment.create(simplePayment).then(function(payment) {
+				done();
+			});
+		});
+	});
+
+	it('should accept 111 as a CVV2', function(done) {
+		simplePayment = _.extend(passingGenericPayment, {
+			cc_number: testCards['Visa'],
+			cc_cvv2: '111'
+		});
+
+		SlidePay.payment.create(simplePayment).then(function(payment) {
+			done();
+		});
+	});
+
+	it('should reject any CVV2 that is not 111', function(done) {
+		simplePayment = _.extend(passingGenericPayment, {
+			cc_number: testCards['Visa'],
+			cc_cvv2: '222'
+		});
+
+		SlidePay.payment.create(simplePayment).fail(function(err) {
+			done();
+		});
+	});
+
+	it('should reject any billing ZIP that is not 11111', function(done) {
+		simplePayment = _.extend(passingGenericPayment, {
+			cc_number: testCards['Visa'],
+			cc_billing_zip: '22222'
+		});
+
+		SlidePay.payment.create(simplePayment).fail(function(err) {
+			done();
+		});
+	});
+});
